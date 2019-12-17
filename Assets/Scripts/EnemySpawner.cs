5d472b8742bd465f1d2e16be9da81027065c7cef
Ahmad Hanif Nurfauzi <hanif_a13@yahoo.com>
2019-12-17 18:03:14 +0700

    update

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject Enemy;
+    // St art is called before the first frame update
+    void Start () {
+        StartCoroutine (StartSpawning ());
+    }
+
+    IEnumerator StartSpawning () {
+        yield return new WaitForSeconds (Random.Range (7f, 10f));
+        Instantiate (Enemy, transform.position, Quaternion.identity);
+        StartCoroutine (StartSpawning ());
+    }
+}
